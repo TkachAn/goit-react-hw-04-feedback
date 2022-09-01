f7af5cf5bfbf2af97ff679f3259fd92f2f8dfec7
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-01 19:00:00 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,54 @@
 import { useState } from 'react';
+import Statistics from './statistics/Statistics';
+import FeedbackOptions from './feedback/FeedbackOptions';
+import Section from './Section/Section';
+import Container from './container/container';
+import Notification from './Notification/Notification';
+import css from './container/container.module.css';
+
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleBtn = (option, func) => {
-    const [option, setState] = useState(0);
-    // func(func => ({
-    //   [option]: func[option] + 1,
-    // }));
+  const handleBtn = option => {
     if (option === 'good') {
-      setGood((option = option + 1));
+      setGood(good + 1);
+    }
+
+    if (option === 'neutral') {
+      setNeutral(neutral + 1);
+    }
+
+    if (option === 'bad') {
+      setBad(bad + 1);
     }
   };
-  // const handleGood = () => {
-  //   good = setGoog(good + 1);
-  // };
-  // const totalFeedback = ({ good, neutral, bad }) => {
-  //   return good + neutral + bad;
-  // };
-  // const positiveFeedbackPercentage = ({ good, neutral, bad }) => {
-  //   return good + neutral + bad > 0 ? (good / (good + neutral + bad)) * 100 : 0;
-  // };
-  // const { good, neutral, bad } = this.state;
-  // const total = totalFeedback(state);
-  // const positivePercentage = Number(
-  //   positiveFeedbackPercentage(state).toFixed(1)
-  // );
+  const total = Number(good + neutral + bad);
+  const percent = Number(total > 0 ? (good / total) * 100 : 0).toFixed(1);
+
   return (
-    // <Container className={css.feedback}>
-    //   <Section title="Please leave feedback" className={css.hh}>
-    //     <div className={css.feed}>
-    //       {/* <h3>Please leave feedback</h3> */}
-    //       <div className={css.pp}>
-    //         <FeedbackOptions
-    //           options={Object.keys(state)}
-    //           onLeaveFeedback={handleBtn}
-    //         />
-    //       </div>
-    //     </div>
-    //   </Section>
-    //   <Section title="Statistics">
-    //     {total ? (
-    //       <Statistics
-    //         good={good}
-    //         neutral={neutral}
-    //         bad={bad}
-    //         total={total}
-    //         percent={positivePercentage}
-    //       />
-    //     ) : (
-    //       <Notification message="There is no feedback" />
-    //     )}
-    //   </Section>
-    // </Container>
-    <div></div>
+    <Container className={css.feedback}>
+      <Section title="Please leave feedback" className={css.hh}>
+        <div className={css.feed}>
+          <div className={css.pp}>
+            <FeedbackOptions onLeaveFeedback={handleBtn} />
+          </div>
+        </div>
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percent={percent}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </Container>
   );
 };
